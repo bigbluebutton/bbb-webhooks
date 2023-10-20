469028222e6173c8abf3a69be595438fab74ac95
mp <miguel@mconf.com>
2023-10-20 16:00:40 -0300

    (xapi) when xapi_enabled==false the Promise is now resolved instead of rejected

--- src/out/xapi/xapi.js
+++ src/out/xapi/xapi.js
@@ -130,7 +130,7 @@ export default class XAPI {
 
         // Do not proceed if xapi_enabled === 'false' was passed in the metadata
         if (meeting_data.xapi_enabled === 'false') {
-          return reject(new Error('xapi is disabled for this meeting'));
+          return resolve();
         }
 
         XAPIStatement = getXAPIStatement(event, meeting_data);
