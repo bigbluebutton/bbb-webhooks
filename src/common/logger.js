af8b60bc6eb14fdf074c4ce97060c0c660491ce2
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2023-10-24 13:25:36 -0300

    chore: replace logger, Winston -> Pino
    Winston@3 is too problematic for a logging library, experiment with Pino for a while to see how it goes

--- src/common/logger.js
+++ src/common/logger.js
@@ -1,90 +1,11 @@
-'use strict';
-
-import { addColors, format, createLogger, transports } from 'winston';
+import pino from 'pino';
 import config from 'config';
-// jsonStringify is an extraneous dependency from Winston
-import jsonStringify from 'safe-stable-stringify';
-
 const LOG_CONFIG = config.get('log') || {};
 const {
   level: DEFAULT_LEVEL,
   filename: DEFAULT_FILENAME,
-  stdout: STDOUT = true
+  stdout: STDOUT = true,
 } = LOG_CONFIG;
-const { combine, colorize, timestamp, json, printf, errors, splat } = format;
-const LEVELS = {
-  error: 0,
-  warn: 1,
-  info: 2,
-  verbose: 3,
-  debug: 4,
-  trace: 5,
-};
-addColors({
-  error: 'red',
-  warn: 'yellow',
-  info: 'green',
-  verbose: 'cyan',
-  debug: 'magenta',
-  trace: 'gray'
-});
-
-/**
- * The logging library used by this module.
- * @name external:winston
- * @external winston
- * @private
- */
-
-/**
- * The logging class exposed by this module.
- * @name external:winston.Logger
- * @memberof external:winston
- * @external winston.Logger
- * @class
- */
-
-/**
- * Method to log a message at a specified level.
- * @name external:winston.Logger#log
- * @memberof external:winston.Logger
- * @function
- * @param {string} level - The log level to use.
- * @param {string} message - The message to log.
- */
-
-/**
- * @typedef {object} BbbWebhooksLogger
- * @property {external:winston.Logger#log} error - log a message at the error level
- * @property {external:winston.Logger#log} warn - log a message at the warn level
- * @property {external:winston.Logger#log} info - log a message at the info level
- * @property {external:winston.Logger#log} verbose - log a message at the verbose level
- * @property {external:winston.Logger#log} debug - log a message at the debug level
- * @property {external:winston.Logger#log} trace - log a message at the trace level
- */
-
-/**
- * _shimmerLoggerWithLabel.
- * @private
- * @param {external:winston.Logger} logger - the logger to be shimmered
- * @param {string} label - the label to be prepended to the message
- * @returns {BbbWebhooksLogger} the shimmered logger
- */
-const _shimmerLoggerWithLabel = (logger, label) => {
-  const shimmeredLogger = Object.assign({}, logger);
-  Object.keys(LEVELS).forEach((level) => {
-    /**
-     * shimmeredLogger[level].
-     * @param {object} message - the message to be logged
-     * @param {string} meta - loggable object to be stringified and appended to the message (metadata)
-     */
-    shimmeredLogger[level] = (message, meta) => {
-      logger.log(level, `[${label}] ${message}`, meta);
-    }
-  });
-
-  return shimmeredLogger;
-};
 
 /**
  * @typedef {object} LoggerOptions
@@ -97,7 +18,7 @@ const _shimmerLoggerWithLabel = (logger, label) => {
  * _newLogger.
  * @private
  * @param {LoggerOptions} options - the options to be used when creating the logger
- * @returns {external:winston.Logger} a Winston logger instance
+ * @returns {external:pino.Logger} a Pino logger instance
  */
 const _newLogger = ({
   filename,
@@ -108,15 +29,14 @@ const _newLogger = ({
 
   if (filename) {
     try {
-      loggingTransports.push(new transports.File({
-        filename,
-        format: combine(
-          timestamp(),
-          splat(),
-          errors({ stack: true }),
-          json(),
-        )
-      }));
+      loggingTransports.push({
+        level,
+        target: 'pino/file',
+        options: {
+          destination: filename,
+          mkdir: true,
+        }
+      });
     } catch (error) {
       // eslint-disable-next-line no-console
       console.error("Failed to create file transport, won't log to file", error);
@@ -126,47 +46,53 @@ const _newLogger = ({
   if (stdout) {
     if (process.env.NODE_ENV !== 'production') {
       // Development logging - fancier, more human readable stuff
-      loggingTransports.push(new transports.Console({
-        format: combine(
-          colorize(),
-          timestamp(),
-          errors({ stack: true }),
-          printf(({ level, message, timestamp, ...meta}) => {
-            const stringifiedRest = jsonStringify(Object.assign({}, meta, {
-              splat: undefined
-            }));
-
-            if (stringifiedRest !== '{}') {
-              return `${timestamp} - ${level}: ${message} ${stringifiedRest}`;
-            } else {
-              return `${timestamp} - ${level}: ${message}`;
-            }
-          }),
-        )
-      }));
+      loggingTransports.push({
+        level,
+        target: 'pino-pretty',
+        colorize: true,
+      });
     } else {
-      loggingTransports.push(new transports.Console({
-        format: combine(
-          timestamp(),
-          splat(),
-          json(),
-          errors({ stack: true }),
-        )
-      }));
+      // Production logging - regular stdout, no colors, no fancy stuff
+      loggingTransports.push({
+        level,
+        target: 'pino/file',
+        options: {
+          destination: 1,
+        },
+      });
     }
   }
 
-  const logger = createLogger({
-    levels: LEVELS,
-    level,
-    transports: loggingTransports,
-    exitOnError: false,
-  });
+  const hooks = {
+    // Reverse the order of arguments for the log method - message comes first,
+    // then the rest of the arguments (object params, errors, ...)
+    logMethod (inputArgs, method) {
+      if (inputArgs.length >= 2) {
+        const arg1 = inputArgs.shift()
+        const arg2 = inputArgs.shift()
+        return method.apply(this, [arg2, arg1, ...inputArgs])
+      }
+      return method.apply(this, inputArgs)
+    }
+  }
 
-  logger.on('error', (error) => {
+  const targets = pino.transport({ targets: loggingTransports }) ;
+
+  targets.on('error', error => {
     // eslint-disable-next-line no-console
-    console.error("Logger failure", error);
-  });
+    console.error('CRITICAL logger failure: ', error.toString());
+
+    if (filename && error.toString().includes('ENOENT') || error.toString().includes('EACCES')) {
+      // eslint-disable-next-line no-console
+      console.error(`CRITICAL: failed to get log file ${filename}`);
+      process.exit(1);
+    }
+  })
+
+  const logger = pino({
+    level,
+    hooks,
+  }, targets);
 
   return logger;
 }
@@ -178,14 +104,6 @@ const BASE_LOGGER = _newLogger({
 });
 
 /**
- * The default logger instance for bbb-webhooks (with label 'bbb-webhooks')
- * @name logger
- * @instance
- * @public
- * @type {BbbWebhooksLogger}
- */
-const logger = _shimmerLoggerWithLabel(BASE_LOGGER, 'bbb-webhooks');
-/**
  * Creates a new logger with the specified label prepended to all messages
  * @name newLogger
  * @instance
@@ -195,8 +113,17 @@ const logger = _shimmerLoggerWithLabel(BASE_LOGGER, 'bbb-webhooks');
  * @returns {BbbWebhooksLogger} the new logger
  */
 const newLogger = (label) => {
-  return _shimmerLoggerWithLabel(BASE_LOGGER, label);
-}
+  return BASE_LOGGER.child({ mod: label });
+};
+
+/**
+ * The default logger instance for bbb-webhooks (with label 'bbb-webhooks')
+ * @name logger
+ * @instance
+ * @public
+ * @type {BbbWebhooksLogger}
+ */
+const logger = newLogger('bbb-webhooks');
 
 export default logger;
 
